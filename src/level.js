3ec032a2053e81592ca877923651a90c98412d6b
Rachael <rachaelewins@hotmail.com>
2020-07-27 09:39:29 +0100

    only add tiles to display that fit

--- src/level.js
+++ src/level.js
@@ -11,9 +11,13 @@ turnIntoTiles(level)
 function drawLevel(gameBox, tileMap, player) {
     tileMap.forEach(tile => {
         const tileDisplay = document.createElement('div')
-        tileDisplay.style.gridRowStart = tile.yPos - player.y + 3
-        tileDisplay.style.gridColumnStart = tile.xPos - player.x + 3
-        tileDisplay.style.backgroundColor = tile.display
-        gameBox.appendChild(tileDisplay)
+        const offsetTileY = tile.yPos - player.y
+        const offsetTileX = tile.xPos - player.x
+        if (offsetTileY > -3 && offsetTileY < 3 && offsetTileX > -3 && offsetTileX < 3) {
+            tileDisplay.style.gridRowStart = tile.yPos - player.y + 3
+            tileDisplay.style.gridColumnStart = tile.xPos - player.x + 3
+            tileDisplay.style.backgroundColor = tile.display
+            gameBox.appendChild(tileDisplay)
+        }
     });
 }
