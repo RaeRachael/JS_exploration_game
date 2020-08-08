00d54e7064300e5b381884cd0a2781bef5b03127
Rachael <rachaelewins@hotmail.com>
2020-08-08 07:49:09 +0100

    Checking player faces correct direction

--- src/player/player.test.js
+++ src/player/player.test.js
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import { updatePlayer, 
   drawPlayer, 
   tileBlocked, 
@@ -123,6 +127,41 @@ describe( "function updatePlayer()", function() {
       var direction = {x:1, y:0}
       updatePlayer(direction, false, tileMap)
       expect(playerLocation).toEqual({ x: 1, y: 1 })
+    })
+
+  })
+
+})
+
+describe( "function drawPlayer()", function() {
+
+  describe( "playerSpace has the correct class codes for css display", function() {
+
+    document.body.innerHTML = '<div id="player" />';
+
+    describe( "should look in the direction of most recent key press", function() {
+      let playerSpace = document.getElementById("player")
+
+      it( "faces up", function() {
+        drawPlayer({ x: 0, y: -1 }, 1)
+        expect(playerSpace.classList[0]).toEqual("up")
+      })
+
+      it( "faces left", function() {
+        drawPlayer({ x: -1, y: 0 }, 1)
+        expect(playerSpace.classList[0]).toEqual("left")
+      })
+
+      it( "faces down", function() {
+        drawPlayer({ x: 0, y: 1 }, 1)
+        expect(playerSpace.classList[0]).toEqual("down")
+      })
+
+      it( "faces right", function() {
+        drawPlayer({ x: 1, y: 0 }, 1)
+        expect(playerSpace.classList[0]).toEqual("right")
+      })
+
     })
 
   })
