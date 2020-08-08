fa91ca06f1deb33e753c8d7a3028f65665ed9387
Rachael <rachaelewins@hotmail.com>
2020-08-08 08:31:46 +0100

    Testing display of Tiles

--- src/level/level.js
+++ src/level/level.js
@@ -10,14 +10,14 @@ const level = ["-----------",
 
 turnIntoTiles(level)
 
-function drawLevel(gameBox, tileMap, player) {
+function drawLevel(gameBox, tileMap, playerLocation) {
     tileMap.forEach(tile => {
-        const offsetTileY = tile.yPos - player.y
-        const offsetTileX = tile.xPos - player.x
+        const offsetTileY = tile.yPos - playerLocation.y
+        const offsetTileX = tile.xPos - playerLocation.x
         if (offsetTileY > -3 && offsetTileY < 3 && offsetTileX > -3 && offsetTileX < 3) {
             const tileDisplay = document.createElement('div')
-            tileDisplay.style.gridRowStart = tile.yPos - player.y + 3
-            tileDisplay.style.gridColumnStart = tile.xPos - player.x + 3
+            tileDisplay.style.gridRowStart = tile.yPos - playerLocation.y + 3
+            tileDisplay.style.gridColumnStart = tile.xPos - playerLocation.x + 3
             tileDisplay.style.backgroundColor = tile.display
             gameBox.appendChild(tileDisplay)
         }
---
+++ src/level/level.test.js
@@ -0,0 +1,38 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { drawLevel } from './level';
+
+describe( "function drawLevel()", function() {
+
+  document.body.innerHTML = '<div id="game-box" />';
+  let gameBox = document.getElementById("game-box")
+
+  var tileMap = [{ blocksPlayer: true, display: "black", xPos: 0, yPos: 0 }, 
+  { blocksPlayer: false, display: "white", xPos: 1, yPos: 0 }]
+  var playerLocation = { x: 1, y: 1}
+
+  it( "adds a child with correct style, based on playerLocation", function() {
+
+    drawLevel(gameBox, tileMap, playerLocation)
+
+    expect(gameBox.children[0].style._values).toEqual(
+      expect.objectContaining({
+        'grid-row-start': 2,
+        'grid-column-start': 2,
+        'background-color': 'black'
+      })
+    )
+
+    expect(gameBox.children[1].style._values).toEqual(
+      expect.objectContaining({
+        'grid-row-start': 2,
+        'grid-column-start': 3,
+        'background-color': 'white'
+      })
+    )
+
+  })
+  
+})
